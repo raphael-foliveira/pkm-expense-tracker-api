8be4f322070b5b04ad266f35e839c940b1555204
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-12-30 12:28:04 -0300

    tests: clear users table before running user repository remove test

--- src/persistence/repository/user.test.ts
+++ src/persistence/repository/user.test.ts
@@ -4,6 +4,7 @@ config({ path: '.env.test' });
 import { userFactory } from '../../stubs/user';
 import { dataSource } from '../data-source';
 import { userRepository } from './user';
+import { User } from '../entitites/user';
 
 describe('UserRepository', () => {
   beforeAll(async () => {
@@ -14,6 +15,7 @@ describe('UserRepository', () => {
     await dataSource.destroy();
   });
   it('should delete a user', async () => {
+    await dataSource.getRepository(User).remove(await userRepository.find({}));
     const createdUser = await userRepository.save(await userFactory());
     const deletedUser = await userRepository.remove(createdUser.id!);
     const foundUsers = await userRepository.find({});

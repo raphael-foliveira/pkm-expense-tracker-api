fa5f3e770b03aa437af883fe950819944c8f878c
Raphael Oliveira <raphael.foliveira1@gmail.com>
2024-03-03 20:17:42 -0300

    refactor: use RequestHandler type instead of typing request and response objects

--- src/http/controller/expense.controller.ts
+++ src/http/controller/expense.controller.ts
@@ -1,40 +1,37 @@
-import { Request, Response } from 'express';
-import { expenseMapper } from './mappers/expense.mapper';
-import { expenseService } from '../../service/expense.service';
+import { RequestHandler } from 'express';
 import { authService } from '../../service/auth.service';
+import { expenseService } from '../../service/expense.service';
+import { expenseMapper } from './mappers/expense.mapper';
 
-const create = async (
-  { body, headers: { authorization } }: Request,
-  res: Response,
+const create: RequestHandler = async (
+  { body, headers: { authorization } },
+  res,
 ) => {
   const { id } = await authService.verifyAccessToken(authorization!);
   const expense = await expenseService.create(body, id!);
   return res.status(201).json(expenseMapper.toResponseDto(expense));
 };
 
-const find = async ({ query }: Request, res: Response) => {
+const find: RequestHandler = async ({ query }, res) => {
   const expenses = await expenseService.find(query);
   return res.status(200).json(expenses.map(expenseMapper.toResponseDto));
 };
 
-const findOne = async ({ params: { id } }: Request, res: Response) => {
+const findOne: RequestHandler = async ({ params: { id } }, res) => {
   const expense = await expenseService.findOne(+id);
   return res.status(200).json(expenseMapper.toResponseDto(expense));
 };
 
-const remove = async (
-  { params: { id }, headers: { authorization } }: Request,
-  res: Response,
+const remove: RequestHandler = async (
+  { params: { id }, headers: { authorization } },
+  res,
 ) => {
   const user = await authService.verifyAccessToken(authorization!);
   await expenseService.remove(+id, user.id!);
   return res.status(204).send();
 };
 
-const getByMonth = async (
-  { params: { month, year } }: Request,
-  res: Response,
-) => {
+const getByMonth: RequestHandler = async ({ params: { month, year } }, res) => {
   const expenses = await expenseService.getByMonth({
     month: +month,
     year: +year,

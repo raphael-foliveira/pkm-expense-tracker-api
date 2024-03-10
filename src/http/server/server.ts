9a75fe11ac9db7abea775173ae2a279f206b4965
Raphael Oliveira <raphael.foliveira1@gmail.com>
2024-03-10 18:32:38 -0300

    refactor: rename datasource properties

--- src/http/server/server.ts
+++ src/http/server/server.ts
@@ -2,16 +2,19 @@ import cors from 'cors';
 import express from 'express';
 import { dataSource } from '../../persistence/data-source';
 import { errorHandlingMiddleware } from '../middleware/error-handling.middleware';
-import { healthcheckRouter } from '../routes/healthcheck.routes';
 import { authRouter } from '../routes/auth.routes';
 import { expenseRouter } from '../routes/expense.routes';
+import { healthcheckRouter } from '../routes/healthcheck.routes';
+import http from 'http';
+import https from 'https';
+import { config } from '../../service/config.service';
 
 export const getApp = () => {
   const app = express();
 
   app.use(express.json());
   app.use(cors());
-  app.use((req, res, next) => {
+  app.use((_, res, next) => {
     res.removeHeader('x-powered-by');
     return next();
   });
@@ -26,7 +29,14 @@ export const getApp = () => {
 
 export const startServer = async () => {
   await dataSource.initialize();
-  return getApp().listen(3000, () => {
-    console.log('listening on port 3000');
+  const app = getApp();
+  if (config.isProductionEnvironment()) {
+    https.createServer(app).listen(443, () => {
+      console.log('Secure server listening on port 443');
+    });
+    return;
+  }
+  http.createServer(app).listen(3000, () => {
+    console.log('Server listening on port 3000');
   });
 };
